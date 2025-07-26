a1575c57dbf095edb4c3bfe8ae47ff5e87ac3605
kunrak <125427166+kunrak@users.noreply.github.com>
2025-07-27 04:51:55 +0530

    Featured Section

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
   plugins: [react(),
         tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0', // Allow external connections
+    port: 5173,      // Explicitly set port
+  },
 })
