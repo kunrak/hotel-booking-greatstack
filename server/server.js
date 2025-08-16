c8fbf670bf4d4dbb570ae60cf414c1354fe3d7d5
kunrak <125427166+kunrak@users.noreply.github.com>
2025-08-16 20:45:44 +0530

    Create Backend of project

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Api is working!!"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
