94bd73c1fb4e8d99425683fc2b84b4d77f826b9d
kunrak <125427166+kunrak@users.noreply.github.com>
2025-08-25 01:17:54 +0530

    setup Clerk authentication in Backend

--- server/server.js
+++ server/server.js
@@ -15,14 +15,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 // Api to listen Clerk Webhooks
-app.use("/api/webhook", clerkWebhooks);
-
-// middleware
-app.use(express.json());
-app.use(clerkMiddleware());
-
-// Api to listen Clerk Webhooks
-app.use("/api/webhook", clerkWebhooks);
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("Api is working!!"));
 
