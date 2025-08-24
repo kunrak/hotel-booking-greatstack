cfc6335a4a960d44bc0fd57203e81f5fbac46844
kunrak <125427166+kunrak@users.noreply.github.com>
2025-08-25 01:22:36 +0530

    setup Clerk authentication in Backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import connectDB from "./configs/db.js";
+import connectDB from "./configs/Db.js";
 import { clerkMiddleware } from "@clerk/express";
-import clerkWebhooks from "./controllers/clerkWebHooks.js";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
@@ -15,14 +15,7 @@ app.use(express.json());
 app.use(clerkMiddleware());
 
 // Api to listen Clerk Webhooks
-app.use("/api/webhook", clerkWebhooks);
-
-// middleware
-app.use(express.json());
-app.use(clerkMiddleware());
-
-// Api to listen Clerk Webhooks
-app.use("/api/webhook", clerkWebhooks);
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("Api is working!!"));
 
